9cf1bb811fd2cbb8c2fb79f46a6860dd59b6b8f1
arish-softdev <arish.softdev@gmail.com>
2026-01-06 21:49:27 +0530

    Fetch in JavaScript

--- apiPratice/fetch3.js
+++ apiPratice/fetch3.js
@@ -1,24 +1,23 @@
 const btn = document.querySelector("#btn");
-const div = document.querySelector(".fetchdiv")
+const div = document.querySelector(".fetchdiv");
 
-btn.addEventListener("click", fun)
+btn.addEventListener("click", fun);
 
 async function fun() {
-    const fetchdata = await fetch("https://dummyjson.com/comments");
-    const convertdata = await fetchdata.json()
+  const fetchdata = await fetch("https://dummyjson.com/comments");
+  const convertdata = await fetchdata.json();
 
-    let output = ""
+  let output = "";
 
-    convertdata.comments.forEach((data) => {
-        output+= `
+  convertdata.comments.forEach((data) => {
+    output += `
         <div class="value">
-            <h2>${ "Body : " + data.body}</h2>
-            <h2>${ "PostId : " + data.postId}</h2>
-            <h3>${ "Likes : " + data.likes}</h3>
-            <h3>${ "User Id : " + data.user.id}</h3>
-            <h3>${ "User Name : " + data.user.fullname}</h3>
-        </div>`
-    });
-    div.innerHTML=output;
-
+            <h2>${"Body : " + data.body}</h2>
+            <h2>${"PostId : " + data.postId}</h2>
+            <h3>${"Likes : " + data.likes}</h3>
+            <h3>${"User Id : " + data.user.id}</h3>
+            <h3>${"User Name : " + data.user.fullname}</h3>
+        </div>`;
+  });
+  div.innerHTML = output;
 }
