57781d651980af164d4f89a5f5afc145dfc409d9
arish-softdev <arish.softdev@gmail.com>
2026-01-06 21:46:27 +0530

    Fetch in JavaScript

--- apiPratice/fetch3.js
+++ apiPratice/fetch3.js
@@ -4,5 +4,22 @@ const div = document.querySelector(".fetchdiv")
 btn.addEventListener("click", fun)
 
 async function fun() {
-    const
+    const fetchdata = await fetch("https://dummyjson.com/comments");
+    const convertdata = await fetchdata.json()
+
+    let output = ""
+
+    convertdata.comments.forEach((data) => {
+        output+= `
+        <div class="value">
+            <h2>${ "Body : " + data.body}</h2>
+            <h2>${ "PostId : " + data.postId}</h2>
+            <h3>${ "Likes : " + data.likes}</h3>
+            <h3>${ "User Id : " + data.user.id}</h3>
+            <h3>${ "User Name : " + data.user.fullname}</h3>
+        </div>`
+    });
+    div.innerHTML=output;
+
 }
+console.log(output);
