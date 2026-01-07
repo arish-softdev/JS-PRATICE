7b5698c2a9b61b98f3aaeac8ab547c0eaa49ef3d
arish-softdev <arish.softdev@gmail.com>
2026-01-07 20:49:19 +0530

    GET in JavaScript

---
+++ apiPratice/get2.js
@@ -0,0 +1,36 @@
+const btn=document.querySelector("#btn");
+    btn.addEventListener("click",getfun)
+
+    function getfun() {
+        fetch("https://6957a070f7ea690182d292af.mockapi.io/user",{
+            method:"GET",
+            headers:{"Content-Type":"application/json"}
+        })
+        .then((res)=>{
+            if (res.ok) {
+                return res.json()
+            }
+            throw new Error("failed to fetch");
+            
+        })
+        .then((data)=>{
+            const box=document.querySelector(".imgbox");
+            box.innerHTML+=""
+             
+            data.forEach((item) => {
+                const img=`
+                <div class="image">
+                  <img src="${item.avatar}" alt="${item.name}"/>
+                  <h3>${"Name : " +item.name}</h3>
+                  <h3>${"Phone : " + item.phone}</h3>  
+                </div>`
+
+                box.innerHTML+=img
+            });
+            
+        })
+        .catch((error)=>{
+            console.log(error.message);
+            
+        })
+    }
