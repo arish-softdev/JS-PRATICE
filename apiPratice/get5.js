87b4bf28e76a921f06d2afcc0b2427b215b5527f
arish-softdev <arish.softdev@gmail.com>
2026-01-12 21:46:47 +0530

    GET in JavaScript

--- apiPratice/get5.js
+++ apiPratice/get5.js
@@ -1,1 +1,16 @@
-const btn = document.querySelector
+const btn = document.querySelector("#btn");
+btn.addEventListener("Click", getfun);
+
+function getfun() {
+    fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user" , {
+        method = "GET",
+        headers = { "Content-Type": "application/json" },
+    })
+    .then ((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error("Failed to Fetch");
+        
+    })
+}
