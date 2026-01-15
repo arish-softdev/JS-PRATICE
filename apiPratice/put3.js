9b451a5093d4ecd20001f52195c1eb84d4146055
arish-softdev <arish.softdev@gmail.com>
2026-01-15 22:00:18 +0530

    PUT in JavaScript

--- apiPratice/put3.js
+++ apiPratice/put3.js
@@ -1,1 +1,23 @@
-let
+let task = [];
+const cardbox = document.querySelector("#cardbox");
+const updateform = document.querySelector("#updateform");
+const form = document.querySelector("#form");
+const editname = document.querySelector("#editname");
+const editemail = document.querySelector("#editemail");
+const editphone = document.querySelector("#phone");
+let currentTaskid = null;
+
+// fetch
+
+function fetchtask() {
+    fetchtask("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user")
+    .then((res) => res.json())
+    .then((data) => {
+        tasks = data;
+        rendercards();
+    })
+    .catch((err) => {
+        console.log("Error : faild to fetch", err);
+        
+    })
+}
