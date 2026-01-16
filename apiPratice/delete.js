f845d2e7c12ce3fa5feb44e60c57deb9c6f832cd
arish-softdev <arish.softdev@gmail.com>
2026-01-16 21:28:31 +0530

    DELETE in JavaScript

--- apiPratice/delete.js
+++ apiPratice/delete.js
@@ -1,28 +1,27 @@
- let tasks=[];
- const cardbox=document.querySelector("#cardbox");
+let tasks = [];
+const cardbox = document.querySelector("#cardbox");
 
- // fetch function start 
- function fetchtask() {
-    fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user")
-    .then((res)=>{
-        return res.json()
+// fetch function start
+function fetchtask() {
+  fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user")
+    .then((res) => {
+      return res.json();
     })
-    .then((data)=>{
-        tasks=data;
-        renderfun()
+    .then((data) => {
+      tasks = data;
+      renderfun();
     })
-    .catch((error)=>{
-        console.log(error);
-        
-    })
- }
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
- // render function start 
- function renderfun() {
-    cardbox.innerHTML=""
-    let output=""
-    tasks.forEach((task)=>{
-        output+=`
+// render function start
+function renderfun() {
+  cardbox.innerHTML = "";
+  let output = "";
+  tasks.forEach((task) => {
+    output += `
         <div class="card">
          <img src="${task.avatar}" alt="${task.name}"/>
          <h2>${task.name}</h2>
@@ -31,36 +30,32 @@
          <button onclick="deleteTask('${task.id}')"/>Delete </button>
          </div>   
            `;
-           cardbox.innerHTML=output
+    cardbox.innerHTML = output;
+  });
+}
+
+// delete function starting
+
+function deleteTask(id) {
+  fetch(`https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      if (res.ok) {
+        tasks = tasks.filter((task) => {
+          return task.id !== id;
+        });
+        renderfun();
+        alert("Task Deleted successfully");
+      } else {
+        throw new Error("Error Deleting Task :", error);
+      }
     })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
- }
+// fetch on page load
 
- // delete function starting
-
- function deleteTask(id) {
-   fetch(`https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user/${id}`,{
-    method:"DELETE",
-   })
-   .then((res)=>{
-    if (res.ok) {
-        tasks=tasks.filter((task)=>{
-            return task.id!==id
-        })
-        renderfun()
-        alert("Task Deleted successfully")
-    }
-    else{
-        throw new Error("Error Deleting Task :",error );
-        
-    }
-   })
-   .catch((error)=>{
-    console.log(error);
-    
-   })
- }
-
- // fetch on page load 
-
- fetchtask()
+fetchtask();
