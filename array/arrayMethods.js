d64b999717de0a3b46494ec97f22396eb4944011
arish-softdev <arish.softdev@gmail.com>
2026-01-30 21:33:27 +0530

    Array Method in JavaScript

--- array/arrayMethods.js
+++ array/arrayMethods.js
@@ -1,41 +1,41 @@
 // Array Method in Javascript
 
-// Push()
+// 1. Push()
 let nums = [1, 2, 3, 4, 5];
 
 nums.push(6);
 console.log(nums);
 
-// Pop()
+// 2. Pop()
 let no = [1, 2, 3, 4, 5];
 
 no.pop()
 console.log(no);
 
-// Map()
+// 3. Map()
 let nos = [1, 2, 3, 4, 5]; 
 
 let doubled = nos.map(n => n * 2);
 console.log(doubled);
 
-// Filter()
+// 4. Filter()
 let number = [1, 2, 3, 4, 5]; 
 
 let even = number.filter(n => n % 2 === 0);
 console.log(even);
 
-// ForEach()
+// 5. ForEach()
 let numbers = [1, 2, 3, 4, 5]; 
 
 numbers.forEach(n => console.log(n));
 
-// Find()
+// 6. Find()
 let nnumbers = [1, 2, 3, 4, 5];
 
 let result = nnumbers.find(n => n > 3);
 console.log(result);
 
-// Reduce()
+// 7. Reduce()
 let nnumber = [1, 2, 3, 4, 5]; 
 
 let output = nnumber.reduce((sum, n) => sum + n, 0);
