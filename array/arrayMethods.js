86130788c4285b9fc8e309d5691e0d803741ebf3
arish-softdev <arish.softdev@gmail.com>
2026-01-30 21:19:43 +0530

    Array Method in JavaScript

--- array/arrayMethods.js
+++ array/arrayMethods.js
@@ -17,3 +17,9 @@ let nos = [1, 2, 3, 4, 5];
 
 let doubled = nos.map(n => n * 2);
 console.log(doubled);
+
+// Filter()
+let number = [1, 2, 3, 4, 5]; 
+
+let even = number.filter(n => n % 2 === 0);
+console.log(even);
